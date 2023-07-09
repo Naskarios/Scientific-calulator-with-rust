25d8f18bb70b53e52fa7d0fa8c4f95a80674e2b2
Naskarios <78686097+Naskarios@users.noreply.github.com>
2023-07-10 01:37:05 +0300

    infix to postfix conversion

--- calc.rs
+++ calc.rs
@@ -2,6 +2,46 @@
 #![allow(unused_parens)]
 use std::io;
 
+
+
+//****************** STACK STRUCT **************************
+//stack implementation I found by googling a bit
+    struct Stack<T> {
+        stack: Vec<T>,
+    }
+  
+        impl<T> Stack<T> {
+
+            fn new() -> Self {
+            Stack { stack: Vec::new() }
+            }
+            
+            fn pop(&mut self) -> Option<T> {
+                self.stack.pop()
+            }
+            
+            fn push(&mut self, item: T) {
+                self.stack.push(item)
+            }
+            
+            fn is_empty(&self) -> bool {
+                self.stack.is_empty()
+            }
+            
+            fn length(&self) -> usize {
+                self.stack.len()
+            }
+        
+            fn peek(&self) -> Option<&T> {
+                self.stack.last()
+            }
+
+            fn peekf(&self) -> Option<&T> { //im not quite sure if the implementation of this functions
+                                            //defeats the whole purpose of using a stack BUT i digress 
+                return self.stack.first();
+            }
+        }
+
 // **************** MAIN FUNCTION ***********************
 
 fn main(){
@@ -11,38 +51,56 @@ fn main(){
     println!("If you need help type help \nIf you want to stop type stop");
 
     let mut input = String::new();
+    let mut postfixBuffer= String::new();
     let mut data="0";
-    let mut buffer= String::new();
-    while("stop"!=data){
+    let mut stack :Stack<char> = Stack::new();
+
+    // stack.push('1');
+    // stack.push('1');   
+    // get 2 numbers from stack and convert the chars to numbers
+    // let test2=stack.pop().unwrap().to_digit(10).unwrap()+stack.pop().unwrap().to_digit(10).unwrap();
+    // println!("EDW !!!!!!!!!!!!!!!! {:?}",test2);
+    
+    // REMINDER TO CLEAR THE BUFFER
+
+    // let test=stack.pop();
+    // let test=stack.pop();
+    // println!("EDW !!!!!!!!!!!!!!!! {:?}",test.unwrap());
+
+    while("stop"!=data){// calculator loop
+
         input.clear();// clears the previous input
         println!("Awaiting input:");
-        io::stdin().read_line(&mut input).unwrap();
+        io::stdin().read_line(&mut input).unwrap(); //reading from keyboard
         data=input.trim();
 
         if ("help" == data) {
-
             menu();
-
         }
         else{
-            
             //Detecting important chars for loop 
+            
             for c in data.chars(){
-                    if (c.is_digit(10) || c=='+' || c=='-' || c=='*' || c=='/' || c=='^' || c=='(' || c==')' ){
-                        buffer.push(c);    //push chars into string ,the method push_str() requires a slice as input
-                                            // push is only for chars
-                        println!("buffer status ->{}",buffer)
-                    }
-                }
-            //a lot of stuff to be done later (buffer?)
+                postfixBuffer=postfixConvert(postfixBuffer,c,&mut stack);
+            }
+            if(!stack.is_empty()){ // leftover operator
+                postfixBuffer.push(stack.pop().unwrap());
+            }
+            println!("FINAL BUFFER {}",postfixBuffer);
+
+                // while(!stack.is_empty()){
+                //     println!("stack empty ->{:?}",stack.pop());
+                // }
+            //a lot of stuff to be done later (postfixBuffer?)
             // buffers evolution is gonna be the infix/postfix stack Credits  to Prof.Giannopoulos at Uni of Peloponnhsoy for teaching me that
         }
     }
-    
+
+
+
 }
 
 // *********************** FUNCTIONS ***************************
-
 fn menu() ->i32{
 
     let menuChoice;
@@ -67,10 +125,73 @@ fn menu() ->i32{
     
     return result;
 }
+
 // **********************************************************
+
 fn listCalc() ->i32{
     println!("Available calulations: \n +,- Addition Subtraction\n *,/ Multilpycation Division\n ^x \"To the power of x\"");
     println!(" sqrt(x) Square root of x\n sin(x),cos(x),tan(x) (x in degrees)\n logX(Y) log of Y with base X");
-    //buffer must be done first
+    //postfixBuffer must be done first
     return 2;
+}
+
+// **********************************************************
+
+fn postfixConvert( mut dataString :String,c :char,stack :&mut Stack<char>)-> String{
+    
+    // if (c.is_digit(10) || c=='+' || c=='-' || c=='*' || c=='/' || c=='^' || c=='(' || c==')' ){
+    if (c=='+' || c=='*' || c=='-' || c=='/' || c=='^'){ 
+        if (stack.is_empty()){ //prevents code panicking from the else if statement below
+            stack.push(c);            
+        }
+        else if ( priority(c) <= priority( *stack.peekf().unwrap() ) ){
+
+            while(!stack.is_empty()){
+                dataString.push(stack.pop().unwrap());
+            }
+            stack.push(c);
+        }
+        else{ 
+            stack.push(c);
+        }
+        
+    }
+
+    if ( c=='('){
+        stack.push(c);
+    }
+    
+    if (c.is_digit(10) ){        
+        //push chars into string with push() ,the method push_str() requires a slice as input                                    
+        dataString.push(c);    
+    }
+
+    if(c==')'){
+
+        let mut temp=stack.pop().unwrap();
+        dataString.push(temp);
+
+        temp= stack.pop().unwrap();
+        while(temp!='('){
+            dataString.push(temp);
+            temp=stack.pop().unwrap();
+        }
+        
+    }
+
+    println!("postfix/dataString status ->{}",dataString);
+    // println!("peek {:?}",stack.peek());
+    return dataString;
+    
+}
+
+// *******************************************************
+
+fn priority( operator :char) -> u32{
+    match operator{
+     '+'|'-'=>return 1,
+     '*'|'/'=>return 2,
+     '^'=>return 3,
+    _=>return 0,
+    }
 }
--- readme.md
+++ readme.md
@@ -2,13 +2,10 @@
 
 ## Description
 
-I always liked the mascot of rust.
+I always liked the mascot of rust. So I decided to make a calculator to get myself familiar with the language.
 <picture>
  <img alt="ferris the crab gesture" src="https://www.rustacean.net/assets/rustacean-flat-gesture.svg">
 So I decided to learn at least the basics of the rust programming language
 by building a calculator 
 
-## Usage
 
-help menu
-</picture>
